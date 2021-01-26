33667fb93fa7f90fcdc1835e136a0adabee92cf4
rusFirstLt <66763500+rusFirstLt@users.noreply.github.com>
2021-01-26 16:45:55 +0900

    Add files via upload

---
+++ TooSimpleFrame.java
@@ -0,0 +1,11 @@
+package lab13;
+
+import java.awt.*;
+
+class TooSimpleFrame extends Frame{
+    public static void main(String[] args) {
+        Frame fr = new TooSimpleFrame();
+        fr.setSize(400,150);
+        fr.setVisible(true);
+    }
+}
